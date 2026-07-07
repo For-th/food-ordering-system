6f5e923a8d2977f78fd501696ad38e96e3cdcce8
For-th <jhonreytuto@gmail.com>
2026-07-07 12:20:19 +0800

    Changed alert to element

--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -35,7 +35,8 @@ function CheckoutPage({ cart, setCart }) {
     // Validate phone number
     const phoneRegex = /^(09\d{9}|\+639\d{9})$/;
     if (!phoneRegex.test(form.phone)) {
-      alert('Please enter a valid phone number');
+      const element = document.querySelector('input[name="phone"]');
+      element.innerHTML = "Please enter a valid phone number";
       return;
     }
 
