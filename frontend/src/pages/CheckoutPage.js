6514fb7e334429d93c921eb5eb3778b1aa30970c
For-th <jhonreytuto@gmail.com>
2026-07-07 12:48:59 +0800

    Error message

--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -35,7 +35,7 @@ function CheckoutPage({ cart, setCart }) {
     // Validate phone number
     const phoneRegex = /^(09\d{9}|\+639\d{9})$/;
     if (!phoneRegex.test(form.phone)) {
-      alert("Please enter a valid phone number");
+      errorMessage.textContent = 'Please enter a valid phone number';
       return;
     }
 
